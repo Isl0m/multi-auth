cb9f5f96cf8ae1f8350fb2ad95e519a9d113ed6f
Isl0m <islomrustam09@gmail.com>
2026-05-06 19:15:42 +0500

    fix ip address

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { Env } from '@/config/configuration';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', true);
 
   const configService = app.get(ConfigService<Env, true>);
 
